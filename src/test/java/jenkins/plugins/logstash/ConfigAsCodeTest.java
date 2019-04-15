11a9ea82f7d1b4a202513fb5b4465d307c9975be
Markus Winter <m_winter@gmx.de>
2019-04-15 13:59:36 +0200

    config as code test for logstash

--- src/test/java/jenkins/plugins/logstash/ConfigAsCodeTest.java
+++ src/test/java/jenkins/plugins/logstash/ConfigAsCodeTest.java
@@ -11,6 +11,7 @@ import org.jvnet.hudson.test.JenkinsRule;
 import io.jenkins.plugins.casc.ConfigurationAsCode;
 import io.jenkins.plugins.casc.ConfiguratorException;
 import jenkins.plugins.logstash.configuration.ElasticSearch;
+import jenkins.plugins.logstash.configuration.Logstash;
 
 public class ConfigAsCodeTest
 {
@@ -29,5 +30,19 @@ public class ConfigAsCodeTest
     ElasticSearch es = (ElasticSearch) c.getLogstashIndexer();
     assertThat(es.getUri().toString(), is("http://localhost:9200/jenkins/test"));
     assertThat(es.getMimeType(), is("application/json"));
+  }
+
+  @Test
+  public void logstash() throws ConfiguratorException
+  {
+    ConfigurationAsCode.get().configure(ConfigAsCodeTest.class.getResource("/jcasc/logstash.yaml").toString());
+    LogstashConfiguration c = LogstashConfiguration.getInstance();
+    assertThat(c.isEnabled(), is(true));
+    assertThat(c.isEnableGlobally(), is(true));
+    assertThat(c.isMilliSecondTimestamps(), is(true));
+    assertThat(c.getLogstashIndexer(), is(instanceOf(Logstash.class)));
+    Logstash logstash = (Logstash) c.getLogstashIndexer();
+    assertThat(logstash.getHost(), is("localhost"));
+    assertThat(logstash.getPort(), is(9200));
   }
 }
